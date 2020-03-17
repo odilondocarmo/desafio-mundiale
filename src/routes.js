7ae6e62f6758cd60f61fb0e2ba49b43cf1eddd15
Odilon do Carmo <54224297+odilondocarmo@users.noreply.github.com>
2020-03-17 19:32:03 -0300

    Add Heroku port and Cors settings

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const {Router, json} = require('express');
+const cors = require('cors');
 const CrawlerMercadoLivreController = require('./controllers/CrawlerMercadoLivreController');
 
 const routes = new Router();
 
+routes.use(cors());
 routes.use(json());
 routes.post('/search', CrawlerMercadoLivreController.index);
 
